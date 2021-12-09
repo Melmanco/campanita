019c80f8c6cedc0a81086adbb3068021ab81ca91
Diego-c-8 <33528388+Diego-c-8@users.noreply.github.com>
2021-12-09 16:31:07 -0300

    Revert "arreglo calendario"
    This reverts commit 9c2e2b292e46711db09ccc2039555181d4be80d5.

--- client/src/pages/Anuncios.js
+++ client/src/pages/Anuncios.js
@@ -4,7 +4,8 @@ import {Calendario} from '../components/Calendario.js'
 function Anuncios(props){
     const {username} = props
     return(
-        <div className="Body">
+        <div>
+            <div className="Body"/>
             <Calendario/>
         </div>
     );

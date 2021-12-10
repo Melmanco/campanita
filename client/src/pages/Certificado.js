dc1ac944625fb3798ef48fd735912471056b1d3d
AnoderPersona <tamibadilla@gmail.com>
2021-12-10 09:15:20 -0300

    fix

--- client/src/pages/Certificado.js
+++ client/src/pages/Certificado.js
@@ -5,15 +5,28 @@ import './Login.css'
 import Logo from '../img/integra.png';
 
 function Certificado(props){
-    const {username} = props
+    const {username} = props;
+    const [perfil,setPerfil] = useState("");
+    const [isRendered, setIsRendered] = useState(false);
+    
     const Enviar = () =>{
 
         Axios.post("http://localhost:8080/send-email",{username: username });
     }
 
+    useEffect(() => {
+        Axios.post("http://localhost:3000/obtener-perfil", {
+            username: username
+        }).then((response) => {
+            console.log(response.data);
+            if(response.data === "Directora" || response.data === "Docente" || response.data === "Estudiante")
+                setPerfil(response.data);
+        });
+        
+        setIsRendered(true);
+    });
     
     return(
-
         <div className="Body">
             <div className = "row container">
                 <div className = "col mt-4">

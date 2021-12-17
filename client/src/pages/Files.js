4b9c9001d33ce9787d07733e52e1a3893e48e099
Felipe Maldonado Abello <felipe.maldonado19@outlook.com>
2021-12-17 09:14:57 -0300

    fix subida de archivos

--- client/src/pages/Files.js
+++ client/src/pages/Files.js
@@ -1,12 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
-import Uploader from '../components/Uploader';
 import '../index.css';
+import Axios from 'axios';
 
 function Files(props){
     const {username} = props;
     const [perfil,setPerfil] = useState("");
-    
+    const [grupo,setGrupo] = useState("");
+    const [file,setFile] = useState(null);
+
+    const handleFile = (e) => {
+        setFile(e.target.files[0]);
+    };
+
+    const handleUpload = (e) => {
+        const formData = new FormData();
+        const mysql_date = new Date(Date.now()).toISOString().slice(0, 19).replace('T', ' ');
+
+        formData.append('file',file);
+        formData.append('grupo',grupo);
+        formData.append('date',mysql_date);
+
+        Axios.post(
+            "http://localhost:3000/send-file",
+            formData,
+            {
+                headers: {
+                    'Content-type': 'multipart/form-data'
+                }
+            }
+        );
+    }
+
     useEffect(() => {
         Axios.post("http://localhost:3000/obtener-perfil", {
             username: username
@@ -15,12 +40,21 @@ function Files(props){
             if(response.data === "Directora" || response.data === "Docente" || response.data === "Estudiante")
                 setPerfil(response.data);
         });
+
+        Axios.post("http://localhost:3000/obtener-grupo",{
+            username: username
+        }).then((response) => {
+            setGrupo(response.data);
+        });
     });
     
     return(
         <div className="Body">
             {perfil === 'Directora' || perfil === 'Docente'?
-            <Uploader/>
+            <div>
+                <input type="file" onChange={(e) => {handleFile(e)}} />
+                <button onClick={(e)=> {handleUpload(e)}}>Enviar</button>
+            </div>
             :
             null
             }
